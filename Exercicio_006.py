0545c4a0d248e6845fce5512400de2235806b8f5
andreFSilva <andrealekhine@gmail.com>
2019-11-17 19:49:58 -0300

    atualizando

---
+++ Exercicio_006.py
@@ -0,0 +1,7 @@
+import math
+num = int(input('Digite um número: '))
+
+print('O doblo de {} vale {}. '.format(num, (num * 2)))
+print('O triplo de {} vale {}. '.format(num, (num * 3)))
+print('A raiz quadrada de {} é igual a {:.2f}. '.format(num, (math.sqrt(num))))
+
---
+++ Exercicio_007.py
@@ -0,0 +1,5 @@
+a = float(input("Primeira nota do aluno: "))
+b = float(input("Segunda nota do aluno: "))
+x = (a + b) / 2
+print('A média entre {} e {} é igual {:.1f}'.format(a, b, x))
+
